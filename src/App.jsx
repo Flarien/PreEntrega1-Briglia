e483a9eb86775791809c69baef528db1290875e2
Flavia S. Briglia <fbrig87@gmail.com>
2023-06-11 12:24:30 -0300

    instala React-DOM y completa el arreglo de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { NavbarContainer } from "./components/layout/NavbarContainer";
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <>
--- src/productsMock.js
+++ src/productsMock.js
@@ -1,53 +1,101 @@
+export const products = [
+  {
+    id: 1,
+    title: "Tulipán",
+    price: 1500,
+    stock: 6,
+    description: "Tulipán (tulplanis politicae)",
+    category: "Exterior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496473/exterior-1_paewfg.webp",
+  },
 
-// export const products = [
-//   {
-//     id: 1,
-//     title: "",
-//     price: ,
-//     stock: 6,
-//     description: "",
-//     category: "interior",
-//     img: ""
-//   },
+  {
+    id: 2,
+    title: "Yucca",
+    price: 3400,
+    stock: 5,
+    description: "Yucca (Asparagaceae)",
+    category: "Exterior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496473/exterior-2_xgw5sz.webp",
+  },
 
-//   {
-//     id: 2,
-//     title: "",
-//     price: ,
-//     stock: 5,
-//     description: "",
-//     category: "interior",
-//     img: ""
-// },
+  {
+    id: 3,
+    title: "Helecho ",
+    price: 2800,
+    stock: 9,
+    description: "Helecho (Dryopteridaceae)",
+    category: "Exterior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496473/exterior-3_brz6qc.webp",
+  },
 
-// {
-//   id: 3,
-//   title: "",
-//   price: ,
-//   stock: 9,
-//   description: "",
-//   category: "interior",
-//   img: ""
-// },
+  {
+    id: 4,
+    title: "Prímula",
+    price: 1200,
+    stock: 5,
+    description: "Prímula (Primuláceae)",
+    category: "Exterior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496474/exterior-4_vc7eea.webp",
+  },
 
-// {
-//   id: 4,
-//   title: "",
-//   price: ,
-//   stock: 5,
-//   description: "",
-//   category: "interior",
-//   img: ""
-// },
+  {
+    id: 5,
+    title: "Margarita Africana",
+    price: 2500,
+    stock: 8,
+    description: "Margarita Africana (Osteospermun fruticosum)",
+    category: "Exterior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496474/exterior-5_ju6k2e.webp",
+  },
 
-// {
-//   id: 5,
-//   title: "",
-//   price: ,
-//   stock: 8,
-//   description: "",
-//   category: "interior",
-//   img: ""
-// },
+  {
+    id: 6,
+    title: "Cuna de Moisés ",
+    price: 4500,
+    stock: 5,
+    description: "Cuna de Moisés (Spathiphyllum)",
+    category: "Interior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496474/interior-1_txru35.webp",
+  },
 
-// ]
+  {
+    id: 7,
+    title: "Alocasia Amazónica",
+    price: 3800,
+    stock: 5,
+    description: "Alocasia Amazónica (Alocasia sanderiana)",
+    category: "Interior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496474/interior-2_psb6je.webp",
+  },
+
+  {
+    id: 8,
+    title: "Palma Areka",
+    price: 4800,
+    stock: 5,
+    description: "Palma Areka (Dypsis lutescens)",
+    category: "Interior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496474/interior-3_x0x0mi.webp",
+  },
+
+  {
+    id: 9,
+    title: "Corazón de Jesús",
+    price: 6400,
+    stock: 5,
+    description: "Corazón de Jesús (Caladium bicolor aiton)",
+    category: "Interior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496475/interior-4_mp4jcy.webp",
+  },
+
+  {
+    id: 10,
+    title: "Calathea",
+    price: 5300,
+    stock: 5,
+    description: "Calathea (Calathea roseopicta)",
+    category: "Interior",
+    img: "https://res.cloudinary.com/dciovdqaf/image/upload/v1686496475/interior-5_x7yuqi.webp",
+  },
+];
